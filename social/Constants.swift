eb68a91e401316e89fac5c65fa9350487f2adc64
SashaEvers <sashaevers@hotmail.com>
2017-04-17 16:38:53 +0100

    sign in ui custom classes complete

---
+++ social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  social
+//
+//  Created by Sasha Evers on 17/04/2017.
+//  Copyright © 2017 Sasha. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
